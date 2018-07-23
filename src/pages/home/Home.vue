bb7588b9754fb66f32ac5d21c11fdf6477fa1f66
hdy <378759617@qq.com>
2018-07-23 15:38:44 +0800

    修改swiper样式

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,44 +1,24 @@
 <template>
   <div>
     <Header></Header>
-    <swiper :options="swiperOption" ref="mySwiper" >
-    <!-- slides -->
-    <swiper-slide><img style="width:100%" src='@/assets/imgs/swiper/1.jpg'></swiper-slide>
-    <swiper-slide><img style="width:100%" src='@/assets/imgs/swiper/2.jpg'></swiper-slide>
-    <swiper-slide><img style="width:100%" src='@/assets/imgs/swiper/3.jpg'></swiper-slide>
-    <swiper-slide><img style="width:100%" src='@/assets/imgs/swiper/4.jpg'></swiper-slide>
-    <swiper-slide><img style="width:100%" src='@/assets/imgs/swiper/5.jpg'></swiper-slide>
-    <swiper-slide><img style="width:100%" src='@/assets/imgs/swiper/6.jpg'></swiper-slide>
-
-    <!-- Optional controls -->
-    <div class="swiper-pagination"  slot="pagination"></div>
-  </swiper>
+    <Swiper></Swiper>
   </div>
 </template>
 
 <script>
 // import Header from './components/Header';
 import Header from "./component/Header";
-import "swiper/dist/css/swiper.css";
-import { swiper, swiperSlide } from "vue-awesome-swiper";
+import Swiper from "./component/Swiper"
 export default {
   name: "Home",
   data() {
     return {
-      swiperOption: {
-        pagination: {
-          el: ".swiper-pagination"
-        },
-        effect: "slide",
-        noSwiping : true,
-        autoplay:true,
-      }
+      
     };
   },
   components: {
     Header,
-    swiper,
-    swiperSlide
+    Swiper
   },
 };
 </script>
---
+++ src/pages/home/component/Swiper.vue
@@ -0,0 +1,48 @@
+<template>
+    <swiper :options="swiperOption" ref="mySwiper" class="swper-inner-pagination">
+    <!-- slides -->
+    <swiper-slide><img class="swiper-img-style"  src='@/assets/imgs/swiper/1.jpg'></swiper-slide>
+    <swiper-slide><img class="swiper-img-style"  src='@/assets/imgs/swiper/2.jpg'></swiper-slide>
+    <swiper-slide><img class="swiper-img-style"  src='@/assets/imgs/swiper/3.jpg'></swiper-slide>
+    <swiper-slide><img class="swiper-img-style"  src='@/assets/imgs/swiper/4.jpg'></swiper-slide>
+    <swiper-slide><img class="swiper-img-style"  src='@/assets/imgs/swiper/5.jpg'></swiper-slide>
+    <swiper-slide><img class="swiper-img-style"  src='@/assets/imgs/swiper/6.jpg'></swiper-slide>
+
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</template>
+
+<script>
+import "swiper/dist/css/swiper.css";
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+export default {
+  name: "Swiper",
+  data() {
+    return {
+      swiperOption: {
+        pagination: {
+          el: ".swiper-pagination"
+        },
+        effect: "slide",
+        noSwiping: true,
+        autoplay: true
+      }
+    };
+  },
+  components: {
+    swiper,
+    swiperSlide
+  }
+};
+</script>
+
+<style scoped lang="stylus">
+.swiper-img-style {
+    width: 100%;
+}
+
+.swiper-pagination >>> .swiper-pagination-bullet-active {
+    background: #fff;
+}
+</style>
