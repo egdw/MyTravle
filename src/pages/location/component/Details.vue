038fc4f3d838a31a5897158e3a9026c7b2d24650
hdy <378759617@qq.com>
2018-07-25 13:33:44 +0800

    位置可以进行搜索了

--- src/pages/location/component/Details.vue
+++ src/pages/location/component/Details.vue
@@ -1,16 +1,20 @@
 <template>
     <div id="main2">
-        <div id="your_location">
+        <div id="your_location" v-show="display">
             你的位置
         </div>
-        <div class="location">
+        <div class="location" v-show="display">
             温州
         </div>
-        <ul class="list_ul" v-for="(item,key,index) of list" :key="index">
+        <ul class="list_ul" v-for="(item,key,index) of list" :key="index" v-show="display">
             <div class="list_title" :id="key">{{key}}</div>
             <li class="location" v-for="l of item" :key="l.id">{{l.name}}</li>
         </ul>
-        <div class="left_controller">
+        <ul class="list_ul2" v-show="!display">
+            <li class="location"  v-for="(item,key,index) of result" :key="index">{{item.name}}</li>
+            <!-- {{result}} -->
+        </ul>
+        <div class="left_controller" v-show="display">
             <ul class="left_controller_ul">
                 <li class="left_controller_li" @click="position(item)" v-for="(item,index) of asci" :key="index">{{item}}</li>               
             </ul>
@@ -20,6 +24,7 @@
 
 <script>
 const axios = require("axios");
+import Bus from "@/assets/js/eventBus.js";
 export default {
   data() {
     return {
@@ -41,18 +46,20 @@ export default {
         "N",
         "O",
         "P",
-        "G",
+        "Q",
+        "R",
+        "S",
+        "T",
         "U",
         "V",
         "W",
         "X",
         "Y",
         "Z"
-      ]
+      ],
+      display: true,
+      result: []
     };
-  },
-  mounted() {
-    this.getdata();
   },
   methods: {
     getdata: function() {
@@ -67,13 +74,51 @@ export default {
     },
     position: function(asci) {
       document.location.hash = asci;
+    },
+    getresult: function(msg) {
+      this.result = [];
+      console.log("接受到..." + msg);
+      for (let i in this.list) {
+        this.list[i].forEach(element => {
+          // console.log(element.name)
+          //   console.log("element.name:"+element.name+element.name.indexOf(msg))
+          if (
+            element.name.indexOf(msg) != -1 ||
+            element.spell.indexOf(msg) != -1
+          ) {
+            console.log(element);
+            this.result.push(element);
+          }
+        });
+      }
     }
+  },
+  mounted() {
+    this.getdata();
+    var self = this;
+    Bus.$on("searchinput", function(msg) {
+      self.display = msg;
+    });
+    // 接受用户输入的内容
+    Bus.$on("textChange", function(msg) {
+      // self.result = msg
+      console.log(msg);
+      if (msg == "") {
+        self.display = true;
+      } else {
+        self.display = false;
+        if (self.timer) {
+          clearTimeout(self.timer);
+          console.log(self.timer);
+        }
+        self.timer = setTimeout(() => self.getresult(msg), 100);
+      }
+    });
   }
 };
 </script>
 
 <style scoped lang="stylus">
-
 .left_controller {
     position: fixed;
     right: 0;
@@ -90,11 +135,11 @@ export default {
 }
 
 .left_controller_li {
-    padding-top: 0.1rem;
+    padding-top: 0.05rem;
 }
 
-.list_ul {
- 
+.list_ul2 {
+    padding-top: 0.88rem;
 }
 
 #your_location {
@@ -114,10 +159,20 @@ export default {
     padding-left: 0.1rem;
     background-color: #edf5f9;
 }
-.list_title:target{
-    padding-top 0.88rem
-    margin-top -0.88rem
+
+.list_title:target {
+    padding-top: 0.88rem;
+    margin-top: -0.88rem;
 }
+
+.location {
+    font-size: 0.15rem;
+    color: #212121;
+    height: 0.44rem;
+    line-height: 0.44rem;
+    padding-left: 0.1rem;
+}
+
 .location {
     font-size: 0.15rem;
     color: #212121;
--- src/pages/location/component/Header.vue
+++ src/pages/location/component/Header.vue
@@ -8,17 +8,40 @@
           城市选择
       </div>
     </div>
-    <div class="locationsearch_main">
+    <div class="locationsearch_main" @click="search">
       <div class="location_input">
-          <span>城市名或拼音(如北京/beijing)</span>
+          <!-- <span>城市名或拼音(如北京/beijing)</span> -->
+          <input v-model="search_text" placeholder="城市名或拼音(如北京/beijing)" />
       </div>
     </div>
 </div>
 </template>
 
 <script>
+import Bus from "@/assets/js/eventBus.js";
 export default {
-  name: "Header"
+  name: "Header",
+  methods: {
+    search: function() {
+      Bus.$emit("searchinput", false);
+    },
+    changeText: function(msg) {
+      // textChange
+      Bus.$emit("textChange", msg)
+    //   setTimeout(() => , 2000);
+    }
+  },
+  data() {
+    return {
+      search_text: ""
+    };
+  },
+  watch: {
+    search_text(msg) {
+       
+        setTimeout(() => this.changeText(msg), 200);
+    }
+  }
 };
 </script>
 
@@ -35,25 +58,29 @@ export default {
     padding-left: 0.1rem;
     padding-right: 0.1rem;
     text-align: center;
+    background-color: #28a1b0;
+    color: #43bfcd;
+    border: 0.01rem;
+    border-radius: 25px;
+    -moz-border-radius: 25px;
 
-    span {
+    input {
         display: block;
         height: 0.3rem;
         line-height: 0.3rem;
+        width: 60%;
+        opacity: 1;
         background-color: #28a1b0;
-        color: #43bfcd;
-        border: 0.01rem;
-        border-radius: 25px;
-        -moz-border-radius: 25px;
         font-size: 0.13rem;
+        margin: 0 auto;
     }
 }
 
 #main {
     position: fixed;
     width: 100%;
-    top 0
-    left 0
+    top: 0;
+    left: 0;
 }
 
 #Header_top {
@@ -73,5 +100,21 @@ export default {
         padding-top: 0.12rem;
         font-size: 0.18rem;
     }
+}
+
+::-webkit-input-placeholder {
+    text-align: center;
+}
+
+:-moz-placeholder { /* Firefox 18- */
+    text-align: center;
+}
+
+::-moz-placeholder { /* Firefox 19+ */
+    text-align: center;
+}
+
+:-ms-input-placeholder {
+    text-align: center;
 }
 </style>
